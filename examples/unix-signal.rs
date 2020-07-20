5a88f22e669569165936d6364b4774e8551157fd
Mahmut Bulut <vertexclique@gmail.com>
2020-07-20 19:57:02 +0200

    Further development

---
+++ examples/unix-signal.rs
@@ -0,0 +1,19 @@
+#[cfg(unix)]
+fn main() -> std::io::Result<()> {
+    use nuclei::*;
+    use std::os::unix::net::UnixStream;
+    use futures::prelude::*;
+
+    drive(async {
+        // Create a Unix stream that receives a byte on each signal occurrence.
+        let (a, mut b) = Handle::<UnixStream>::pair()?;
+        signal_hook::pipe::register(signal_hook::SIGINT, a)?;
+        println!("Waiting for Ctrl-C...");
+
+        // Receive a byte that indicates the Ctrl-C signal occurred.
+        b.read_exact(&mut [0]).await?;
+
+        println!("Done!");
+        Ok(())
+    })
+}
